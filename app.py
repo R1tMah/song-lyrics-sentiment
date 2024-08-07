785056cd4929a3dd104af7937ac6efcb1cdd55d6
R1tMah <rimahapatra7@gmail.com>
2024-08-07 05:54:40 -0400

    started model process

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from keras.utils import pad_sequences
+
 
 
 
@@ -29,3 +32,12 @@ data['Lyrics'] = data['Lyrics'].apply(lambda x: x.lower())
 data['Lyrics'] = data['Lyrics'].apply(lambda x: ''.join([c for c in x if c.isalpha() or c.isspace()]))
 data = data[['Name', 'Artist', 'Lyrics']]
 data.to_csv("data/filteredData.csv", index = False)
+
+tokenizer = Tokenizer(num_words= 10000, oov_token= "<OOV>")
+tokenizer.fit_on_texts(data['Lyrics'])
+sequences = tokenizer.texts_to_sequences(data['Lyrics'])
+padded_sequences = pad_sequences(sequences, maxlen=100, truncating='post', padding='post')
+
+labels = pd.get_dummies(data['sentiment']).values
+
+X_train, X_test, y_train, y_test = train_test_split(padded_sequences, labels, test_size=0.2, random_state=42)
